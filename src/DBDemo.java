c69014cac0654ef1b39417f2328f84949af48332
Your Name <emmyli119@gmail.com>
2017-11-18 02:47:50 -0800

    add start class

---
+++ src/DBDemo.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class DBDemo {
+	public static void main(String[] args){
+	
+	try{
+	Connection conn = DriverManager.getConnection("");
+	}
+	catch(e exception){
+		
+	}
+	}
+	
+}
